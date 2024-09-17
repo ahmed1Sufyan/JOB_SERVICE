beee25a25917140bcbaec6281fe1d7349c4468d3
sufyan <csc21s135@gmail.com>
2024-09-17 14:52:50 +0500

    setup express

---
+++ src/config/index.ts
@@ -0,0 +1,31 @@
+// import { configDotenv } from 'dotenv';
+// import path from 'path';
+
+// configDotenv();
+// configDotenv({
+//     path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`),
+// });
+
+const {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    RefreshToken_SecretKey,
+    JWKS_URI,
+} = process.env;
+
+export const Config = {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    RefreshToken_SecretKey,
+    JWKS_URI,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
-function all(a: number) {
-    const result = {
-        prop: a,
-        result: 2,
-        message: 'All passed',
-    };
-    // eslint-disable-next-line no-console
-    console.log(result.message);
+import { log } from 'console';
+import app from './app';
+// import { HttpError } from "http-errors".
 
-    return result['message'];
-}
-all(2);
+const startServer = () => {
+    try {
+        app.listen(8000, () => log('server is listening on port 8000'));
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            log.error(`Error starting server: ${error.message}`);
+        }
+        process.exit(1);
+    }
+};
+
+startServer();
